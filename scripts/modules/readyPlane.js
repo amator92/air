1210e5a44b7f1ca8ce65271c3f84b664476656a3
Oleksandr <amator92@ukr.net>
2022-02-02 12:33:58 +0200

    day2

--- scripts/modules/readyPlane.js
+++ scripts/modules/readyPlane.js
@@ -1,4 +1,4 @@
-
+import airplane from "./airplane.js";
 
 const readyPlane = (forms, main) => {
   const data = [];
@@ -15,7 +15,11 @@ const readyPlane = (forms, main) => {
         name: form.name.value,
         ticket: form.ticket.value,
       });
-      console.log('data:', data)
+
+      if(forms.length === data.length) {
+        forms.forEach(form => form.remove());
+        airplane(main, data);
+      }
     });
   });
 };
